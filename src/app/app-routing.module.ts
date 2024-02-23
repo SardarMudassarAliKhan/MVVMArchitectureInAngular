7bf4a8079fb7710794a32717a886203689eb1b70
mudassarali.official <mudassarali.official@gmail.com>
2024-02-24 04:00:25 +0500

    Lazy Loading Implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'Login', pathMatch: 'full'},
+  { path: 'developer', loadChildren: () => import('./developer/developer.module').then(m => m.DeveloperModule) },
+  { path: 'public-user', loadChildren: () => import('./public-user/public-user.module').then(m => m.PublicUserModule) },
+  { path: 'manager', loadChildren: () => import('./manager/manager.module').then(m => m.ManagerModule) },
+  { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
